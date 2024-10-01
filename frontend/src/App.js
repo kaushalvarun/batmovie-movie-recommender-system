9a934a1e27fbe416fa46b16c81d6b6d62033d141
KaushalCorp <kaushalvarunkv@gmail.com>
2024-10-01 23:32:02 +0530

    Removed unnecessary imports in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import About from './About';
