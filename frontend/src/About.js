5902963d9d48cb2e4361a8bae880e0d215385a83
KaushalCorp <kaushalvarunkv@gmail.com>
2024-10-01 23:36:29 +0530

    Updated production build and removed unneccesary imports

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import './About.css';
